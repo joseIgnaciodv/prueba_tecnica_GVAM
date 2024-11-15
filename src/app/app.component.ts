6fdae9a74f25609010489dba07a7955214464abb
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2024-11-15 21:55:03 +0100

    implement the handle value change from the child component(modal) in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,21 @@ import { ChangeFontSizeService } from './services/change-font-size.service';
 })
 export class AppComponent {
   title = 'prueba_tecnica_GVAM';
+  open: boolean = false; // Variable encargada de determinar si esta abierto o cerrado el modal
 
   constructor(private changeSize: ChangeFontSizeService) {}
 
+  // Función encargada de abrir el modal, poniendo el valor de 'open' a verdadero
+  openModal() {
+    this.open = true;
+  }
+
+  // Función encargada de tratar el cambio de valor de la variable 'open', desde el componente hijo (modal - component)
+  handleToggle(value: boolean) {
+    this.open = value;
+  }
+
+  // Función encargada de incrementar el tamaño de la fuente, llamando a la función (increaseFontSize) del servicio
   increaseFontSize() {
     this.changeSize.increaseFontSize();
     let fontSize = this.changeSize.getFontSize();
@@ -18,6 +30,7 @@ export class AppComponent {
       fontSize + 'px';
   }
 
+  // Función encargada de reducir el tamaño de la fuente, llamando a la función (decreaseFontSize) del servicio
   decreaseFontSize() {
     this.changeSize.decreaseFontSize();
     let fontSize = this.changeSize.getFontSize();
